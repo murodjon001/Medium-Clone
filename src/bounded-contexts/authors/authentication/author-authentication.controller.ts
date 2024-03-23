aec07fb659f63f6e66ce10b74d721a27a0b6685d
murodjon001 <shermuhamedovmurodjon@gmail.com>
2024-03-23 09:16:06 +0500

    fix: minor

--- src/bounded-contexts/authors/authentication/author-authentication.controller.ts
+++ src/bounded-contexts/authors/authentication/author-authentication.controller.ts
@@ -1,16 +1,14 @@
-import { Controller, Post, UseGuards } from "@nestjs/common";
-import { AuthorAuthenticationService } from "./author-authentication.service";
-import { AuthorJwtGuard } from "src/infrastucture/security/guards/author/author-jwt-auth.guard";
-import { GetCurrentUser } from "src/infrastucture/decorators/get-current-user";
-import { AuthorEntity } from "../entity/author.entity";
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthorAuthenticationService } from './author-authentication.service';
+import { AuthorJwtGuard } from 'src/infrastucture/security/guards/author/author-jwt-auth.guard';
+import { GetCurrentUser } from 'src/infrastucture/decorators/get-current-user';
+import { AuthorEntity } from '../entity/author.entity';
 
 @Controller('author/authentication')
 export class AuthorAuthenticationController {
-    constructor(private readonly service: AuthorAuthenticationService){}
+  constructor(private readonly service: AuthorAuthenticationService) {}
 
-    @UseGuards(AuthorJwtGuard)
-    @Post('login')
-    login(@GetCurrentUser() user: AuthorEntity){
-        
-    }
+  @UseGuards(AuthorJwtGuard)
+  @Post('login')
+  login(@GetCurrentUser() user: AuthorEntity) {}
 }
